6bc9f1d16a5e5b73d5d809d40140f01318a208b8
Sírius Gomes <siriusvz@hotmail.com>
2017-07-10 21:31:42 -0300

    v1.

--- tracker/ViewController.swift
+++ tracker/ViewController.swift
@@ -2,24 +2,88 @@
 //  ViewController.swift
 //  tracker
 //
-//  Created by Sírius Gomes on 10/07/17.
+//  Created by Sírius Gomes on 09/07/17.
 //  Copyright © 2017 Sírius Gomes. All rights reserved.
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+ 
+    
+    let locationManager = CLLocationManager()
+   
+    @IBOutlet weak var enviadosLabel: UILabel!
+    @IBOutlet weak var lngLabel: UILabel!
+    @IBOutlet weak var latLabel: UILabel!
+    var enviados = 0
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            //locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        lngLabel.text = "\(locValue.longitude)"
+        latLabel.text = "\(locValue.latitude)"
+        
+        // Coloca o pin no mapa..
+        let annotation = MKPointAnnotation()
+        annotation.title = "eh nois!"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: locValue.latitude, longitude: locValue.longitude)
+        mapView.addAnnotation(annotation)
+        
+        var request = URLRequest(url: URL(string: "http://tracker.siriusgomes.com.br/index.php")!)
+        request.httpMethod = "POST"
+        let postString = "lat=\(locValue.latitude)&lng=\(locValue.longitude)"
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(String(describing: error))")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(String(describing: responseString))")
+            
+            //self.enviadosLabel.text = "\(self.enviados)"
+            //self.enviados += 1
+        }
+        task.resume()
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
